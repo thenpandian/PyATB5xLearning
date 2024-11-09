e3dc8c652a02dda1302ce24057ec70de06879a8b
Thenpandian <thenpandian@gmail.com>
2024-11-09 08:19:35 +0530

    print09112024

---
+++ Nov/ex_09112024_Literals_variables/Lab016.py
@@ -0,0 +1,5 @@
+age = 65  #65  is literals
+
+age+1
+
+print(age)   # Still age is 65, if you  age+1 reassign then only it will increase
---
+++ Nov/ex_09112024_Literals_variables/Lab024.py
@@ -0,0 +1,8 @@
+age = "90"
+age1 = 90
+print(type(age))
+print(type(age1))
+
+# str(), int(), float()
+
+print(type(int(age)))
